548f9452d4d8e5ca052db978d95825c088653ea1
aarol <aarol@epbyminw1962.minsk.epam.com>
2015-02-19 13:26:21 +0300

    Fix JSON template for logstash config file.

--- logstash_start.sh
+++ logstash_start.sh
@@ -9,13 +9,13 @@ cat > ${LOGSTASH_PATH}/consume.conf <<EOF
             ack => true
             durable => true
             host => "feed"
-            port => "$FEED_PORT_5672_TCP_PORT"
+            port => $FEED_PORT_5672_TCP_PORT
             user => "$RMQ_USER"
             password => "$RMQ_PASS"
             exchange => "$RMQ_EXCHANGE"
             queue => "$RMQ_QUEUE_NAME"
             key => "$RMQ_ROUTING_KEY"
-            prefetch_count => "$RMQ_PREFETCH_COUNT"
+            prefetch_count => $RMQ_PREFETCH_COUNT
             vhost => "$RMQ_VHOST"
         }
     }
